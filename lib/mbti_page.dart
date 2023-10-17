35e66724734e6a56b8ca40e110d3bff8c51d4943
Chaemin Jang <jchaemin@Chaemins-MacBook-Pro.local>
2023-10-17 22:49:25 +0900

    fixed main page

--- lib/mbti_page.dart
+++ lib/mbti_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:size_config/size_config.dart';
+import 'package:howami/mbti_select.dart';
 
 
 class mbtiPage extends StatelessWidget {
@@ -15,30 +16,86 @@ class mbtiPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
+          padding: EdgeInsets.fromLTRB(0, 80.w, 0, 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
-                padding: EdgeInsets.fromLTRB(25.w, 0, 0, 0),
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10.w),
                 child: Text(
                   '당신의 MBTI는?',
                   style: TextStyle(
                       color: Colors.black,
-                      fontWeight: FontWeight.w700,
-                      fontSize:18.w
+                      fontWeight: FontWeight.w900,
+                      fontSize:25.w
                   ),
                   maxLines: 2,
                 ),
               ),
               Container(
-                height: 40.w,
+                height: 30.w,
               ),
               Padding(
-                padding: EdgeInsets.fromLTRB(0.w, 0, 0.w, 0),
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10.w),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
+                    mbtiSelect(title: 'ENFP'),
+                    mbtiSelect(title: 'ENFJ'),
+                    mbtiSelect(title: 'ENTJ'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    mbtiSelect(title: 'ENTP'),
+                    mbtiSelect(title: 'ESFP'),
+                    mbtiSelect(title: 'ESFJ'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    mbtiSelect(title: 'ESTJ'),
+                    mbtiSelect(title: 'ESTP'),
+                    mbtiSelect(title: 'INFP'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    mbtiSelect(title: 'INFJ'),
+                    mbtiSelect(title: 'INTP'),
+                    mbtiSelect(title: 'INTJ'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    mbtiSelect(title: 'ISFP'),
+                    mbtiSelect(title: 'ISFJ'),
+                    mbtiSelect(title: 'ISTP'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    mbtiSelect(title: 'ISTJ'),
                   ],
                 ),
               ),
