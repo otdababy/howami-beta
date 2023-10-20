83ad26c70c2d0003e3bcd92bbee02549e566449e
sunshade8 <stevenpark119@gmail.com>
2023-10-20 16:59:35 +0900

    font

--- lib/main_new.dart
+++ lib/main_new.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
+            fontFamily: 'SnowCrab',
             // This is the theme of your application.
             //
             // Try running your application with "flutter run". You'll see the
